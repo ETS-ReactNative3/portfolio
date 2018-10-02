61e096495c50678f1091ef40d93cd1e50ddeacd9
Jackie <jackie@pen.do>
2018-10-01 20:07:00 -0700

    added images to work

--- src/components/project_triangle.js
+++ src/components/project_triangle.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Image1 from './../assets/triangle/1.jpg';
 
 class ProjectTriangle extends Component {
   constructor(props, context) {
@@ -8,8 +9,15 @@ class ProjectTriangle extends Component {
 	render() {
 		return (
 			<div>
-        this is the triangle project
-      </div>
+				<img src={Image1} />
+				<p>
+					Tri-Angle is 3 triangular/modular lamps that can be placed at different angles. Tri-Angle consists of three tri-angular shaped pieces that lights up when another modular piece is connected with it.
+				</p>
+				<p>
+					Please check out our team blog for a more indepth look of the process and behind the scenes!
+				</p>
+
+				</div>
 		)
 	}
 }
